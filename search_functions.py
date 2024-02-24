2ac7b185551734e97851f6d014122b58af7f9a63
Gildian Gonzales <gildiangonzales@gmail.com>
2024-02-24 17:47:28 -0500

    added beam optimization

--- search_functions.py
+++ search_functions.py
@@ -2,7 +2,7 @@ import heapq
 from helpers import get_closest_links,get_closest_link, spinner, DEBUG_MODE
 import sys
 
-def best_first_search(start_page, goal_page):
+def best_first_search(start_page, goal_page, beam_width=3):
     queue = []
     heapq.heappush(queue, (0, [start_page]))  # priority queue to store the pages to be explored
     visited = set()  # set to store the visited pages
@@ -11,18 +11,19 @@ def best_first_search(start_page, goal_page):
         sys.stdout.write('\b')
         sys.stdout.write(next(spinner))
         sys.stdout.flush()  # code to add a spinner to the console so the user can tell its working
-        score, current_page_list = heapq.heappop(queue)  # get the page with the highest priority
-        if current_page_list[-1].lower() == goal_page.lower():  # if the current page is the goal page, return the path
-            return current_page_list
-        current_page = current_page_list[-1].lower()
-        if current_page not in visited:  # if the current page has not been visited
-            visited.add(current_page)  # mark the current page as visited
-            closest_links = get_closest_links(current_page, goal_page, current_page_list)  # find the next best page
-            for links in closest_links:
-                new_score = score + -links[1]
-                new_list = current_page_list.copy()
-                new_list.append(links[0])
-                heapq.heappush(queue, (new_score, new_list))  # add the next page to the priority queue
+        current_pages = heapq.nsmallest(beam_width, queue)  # get the pages with the highest priority
+        for score, current_page_list in current_pages:
+            if current_page_list[-1].lower() == goal_page.lower():  # if the current page is the goal page, return the path
+                return current_page_list
+            current_page = current_page_list[-1].lower()
+            if current_page not in visited:  # if the current page has not been visited
+                visited.add(current_page)  # mark the current page as visited
+                closest_links = get_closest_links(current_page, goal_page, current_page_list)  # find the next best page
+                for links in closest_links:
+                    new_score = score + -links[1]
+                    new_list = current_page_list.copy()
+                    new_list.append(links[0])
+                    heapq.heappush(queue, (new_score, new_list))  # add the next page to the priority queue
     print("QUEUE EMPTY WITHOUT SOLUTION")
     exit()
 
