5c42d7ba75dcdda14c3e66a04c8593ddbb05f48a
Steve <stevetuozzo@gmail.com>
2024-03-01 21:28:25 -0500

    Added frontend for wiki bot WIP Separated bot files into folder 'Bot'

---
+++ client/src/views/Home/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.scss';
+
+function Home() {
+  return (
+    <div>
+      <h1>The Wiki Bot</h1>
+      <form action=""></form>
+      <section></section>
+      <div className="wikiPages">
+        <iframe src="" frameborder="0"></iframe>
+        <iframe src="" frameborder="0"></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default Home
