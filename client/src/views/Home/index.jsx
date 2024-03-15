8ec9cf3fe45f8bc4a2844b308ed2018c1d73abb5
Steve <stevetuozzo@gmail.com>
2024-03-14 23:39:41 -0400

    Client functional UI incomplete

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,41 +1,131 @@
 import React, { useEffect, useMemo, useState } from "react";
+import axios from 'axios';
 import "./styles.scss";
 
 function Home() {
-  const [clicks, setClicks] = useState('3');
-  const [timeTaken, setTimeTaken] = useState('Press Go!');
-  const [pathData, setPathData] = useState(['Apple TV', 'Papa Johns', 'Pizza']);
-  const [loading, setLoading] = useState(false);
-  const [startTitle, setStartTitle] = useState('Minecraft');
-  const [endTitle, setEndTitle] = useState('Pizza');
+  const [pathData, setPathData] = useState({
+    path: "[]",
+    timeTaken: 0,
+  });
+  const [loading, setLoading] = useState(true);
+  const [startTitle, setStartTitle] = useState("Minecraft");
+  const [endTitle, setEndTitle] = useState("Pizza");
+  const [algoToggle, setAlgoToggle] = useState("best_first_search");
+  const [fact, setFact] = useState("");
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [factChangerId, setFactChangerId] = useState(null);
 
-  // this changes once data is being loaded/bot running
-  const [newPage, setNewPage] = useState(true);
+  const clicks = useMemo(() => {
+    let pathArr = pathData.path.replaceAll("'", '"');
+    pathArr = pathArr.replaceAll('"s', "'s");
+    pathArr = JSON.parse(pathArr);
+    return pathArr.length - 1;
+  }, [pathData]);
 
-  const path = useMemo((() => {
-      let text = '';
-      let counter = pathData.length;
-      pathData.forEach(element => {
-        if (counter === 1) text += `${element}`
-        else text += `${element} > `
-        counter--;
+  const path = useMemo(() => {
+    let pathArr = pathData.path.replaceAll("'", '"');
+    pathArr = pathArr.replaceAll('"s', "'s");
+    pathArr = JSON.parse(pathArr);
+    let text = "";
+    let counter = pathArr.length;
+    pathArr.forEach((element) => {
+      if (counter === 1) text += `${element}`;
+      else text += `${element} > `;
+      counter--;
+    });
+    return text;
+  }, [pathData]);
+
+  const algoTitle = useMemo(() => {
+    switch (algoToggle) {
+      case "best_first_search":
+        return "Best";
+      case "depth_first_search":
+        return "Depth";
+    }
+  }, [algoToggle]);
+
+  const fetchFactData = async () => {
+    try {
+      const res = await axios.get('https://api.api-ninjas.com/v1/facts?limit=1', {
+        headers: {
+          'X-Api-Key': 'qKYv6ffmUut4HXbt/CswWA==HIDFaMPKP5qaj5De'
+        }
       });
-      return text;
-    }), [pathData])
-
-  //const [wiki1, setWiki1] = useState('Fun Fact:')
+      setFact(res.data[0].fact);
+    } catch (error) {
+      console.log(`Error: ${error.message}`)
+      setFact("I couldn't think of a good one")
+    }
+  }
 
   useEffect(() => {
-    //if (loading) {
+    if (firstLoad) {
+      fetchFactData()
+      setLoading(false);
+    }
+  }, [firstLoad])
 
-    //}
-  
-  }, [])
+  useEffect(() => {
+    if (loading && !firstLoad) {
+      fetchFactData();
+      const factChanger = setInterval(() => {
+        fetchFactData();
+      }, 10000);
+      setFactChangerId(factChanger)
+    }
+    return () => {
+      if (factChangerId) {
+        clearInterval(factChangerId);
+      }
+    }
+  }, [loading]);
+
+  const fetchSearchData = async (isRandom) => {
+    try {
+      const res = await axios.post('http://localhost:4000/', {
+          isRandom: isRandom,
+          startArticle: startTitle,
+          endArticle: endTitle,
+          algo: algoToggle
+      });
+      let results = res.data.results;
+      if (results.length === 2) {
+        results = results.find(result => result.algo === "best_first_search");
+        setPathData({
+          path: results.path,
+          timeTaken: results.time
+        });
+      }
+      else {
+        setPathData({
+          path: results[0].path,
+          timeTaken: results[0].time
+        });
+      }
+    } catch (error) {
+      console.log(`Error: ${error.message}`)
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('test submit')
-  }
+    setLoading(true);
+    await fetchSearchData(e.target.value === 'random' ? true : false);
+    setFirstLoad(false);
+    setLoading(false);
+  };
+
+  const handleAlgoToggle = (e) => {
+    switch (algoToggle) {
+      case "best_first_search":
+        setAlgoToggle("depth_first_search");
+        break;
+      case "depth_first_search":
+        setAlgoToggle("best_first_search");
+        break;
+    }
+  };
 
   return (
     <div className="homePage">
@@ -44,25 +134,50 @@ function Home() {
         <div></div>
         <div className="centerSection">
           <form id="pathForm" onSubmit={(e) => handleSubmit(e)}>
-            <input type="text" placeholder="Start Title" name="startTitle" value={startTitle} onFocus={() => setStartTitle('')} onChange={(e) => setStartTitle(e.target.value)}/>
+            <input
+              type="text"
+              placeholder="Start Title"
+              name="startTitle"
+              value={startTitle}
+              onFocus={() => setStartTitle("")}
+              onChange={(e) => setStartTitle(e.target.value)}
+            />
             <p>to</p>
-            <input type="text" placeholder="End Title" name="endTitle" value={endTitle} onFocus={() => setEndTitle('')} onChange={(e) => setEndTitle(e.target.value)}/>
-            <button className="goButton" type="submit">Go</button>
-            <button type="submit">Random</button>
+            <input
+              type="text"
+              placeholder="End Title"
+              name="endTitle"
+              value={endTitle}
+              onFocus={() => setEndTitle("")}
+              onChange={(e) => setEndTitle(e.target.value)}
+            />
+            <button className="algoToggleButton" type="button" onClick={handleAlgoToggle}>
+              {algoTitle} First Search
+            </button>
+            <button className="goButton" type="submit" value="go">
+              Go
+            </button>
+            <button type="submit" value="random">Random</button>
           </form>
           <section className="botInfo">
             <div className="infoHeader">
-              <p className="clicks">{clicks} Clicks</p>
-              <p className="timeTaken">{timeTaken}</p>
+              <p className="clicks">{firstLoad ? '*' : clicks} Clicks</p>
+              <p className="timeTaken">{firstLoad ? 'Time: * Seconds' : `Time: ${pathData.timeTaken} Seconds`}</p>
             </div>
             <div className="pathInfo">
-              <p className="path">{path}</p>
+              <p className="path">{firstLoad ? <>Click <span>Go</span> or <span>Random</span> to start your search.<br/>Click <span>{algoTitle} First Search</span> to toggle search modes.</> : path}</p>
             </div>
           </section>
-          <div className="wikiPages">
-            <iframe src="" frameborder="0"></iframe>
-            <iframe src="" frameborder="0"></iframe>
-          </div>
+          {loading || firstLoad ? (
+            <div className="funFact">
+              <p>Fun Fact:<br/>{fact ? fact : 'Loading...'}</p>
+            </div>
+          ) : (
+            <div className="wikiPages">
+              <iframe src="" frameborder="0"></iframe>
+              <iframe src="" frameborder="0"></iframe>
+            </div>
+          )}
         </div>
         <div></div>
       </main>
